8e75b4143d3b635c46d8211ffc0bed9d3b76e04a
Andy Giles <andyg5000@gmail.com>
2015-09-07 14:25:26 -0400

    Initial commit of Asana Kanban

---
+++ blueoakanban.js
@@ -0,0 +1,254 @@
+Issues = new Mongo.Collection("issues");
+AsanaUsers = new Mongo.Collection("asana_users");
+AsanaWorkspaces = new Mongo.Collection("asana_workspaces");
+AsanaTasks = new Mongo.Collection("asana_tasks");
+
+
+if (Meteor.isClient) {
+
+  Meteor.subscribe('asana_tasks');
+  Meteor.subscribe('asana_workspaces');
+
+  Template.dashboard.helpers({
+    issues: function() {
+      var workspace = parseInt(Session.get('Workspace'));
+      if (workspace) {
+        return AsanaTasks.find({completed: false, 'workspace.id': workspace});
+      }
+      else {
+        return AsanaTasks.find({completed: false});
+      }
+    },
+  });
+
+  Template.dashboard.events({
+
+  });
+
+  Template.issue.helpers({
+
+  })
+
+  Template.filters.helpers({
+    workspaces: function() {
+      return AsanaWorkspaces.find({});
+    }
+  });
+
+  Template.filters.events({
+    'change .workspace-filter' : function(event) {
+      event.preventDefault();
+      Session.set('Workspace', event.target.value);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Accounts.onLogin(function() {
+    Meteor.call('userLogin');
+  });
+}
+
+
+if (Meteor.isServer) {
+
+  // Initailizes an asana api oauth connection.
+  Meteor.asanaClient = function() {
+    var user = Meteor.user();
+    var asanaService = ServiceConfiguration.configurations.findOne({service:'asana'});
+    if (user && asanaService) {
+      var client = Asana.createClient({
+        oauth: {
+          clientId: asanaService.clientId,
+          clientSecret: asanaService.clientSecret,
+          accessToken: user.services.asana.accessToken,
+          refreshTokan: user.services.asana.refreshToken,
+          redirectUrl: Meteor.absoluteUrl("_oauth/asana")
+        }
+      });
+      return client;
+    }
+    return null;
+  }
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  Meteor.publish('asana_tasks', function () {
+    if (this.userId) {
+      return AsanaTasks.find({'assignee.id' : 4216372728453});
+    }
+    return [];
+  });
+
+  Meteor.publish('asana_workspaces', function() {
+    if (this.userId) {
+      return AsanaWorkspaces.find({});
+    }
+    return [];
+  });
+
+  Meteor.methods({
+    asanaUsers: function asanaUsers() {
+      var asanaClient = Meteor.asanaClient();
+      var users = Async.runSync(function(done) {
+        asanaClient.users.list(function(err, users) {
+            done(null, users);
+        });
+      })
+
+      _.each(users.result, function(user) {
+        console.log(user.name);
+        if (user.id) {
+          AsanaUsers.upsert({
+            id: user.id
+          }, {
+            $set: {
+              id: user.id,
+              name: user.name
+            }
+          });
+        }
+      });
+      // console.log(users.result);
+      // return users.result;
+    },
+    asanaWorkspaces: function asanaWorkspaces() {
+      var asanaClient = Meteor.asanaClient();
+      var workspaces = Async.runSync(function(done) {
+        asanaClient.workspaces.list(function(err, workspaces) {
+          done(null, workspaces);
+        });
+      });
+
+      _.each(workspaces.result, function(workspace) {
+        if (workspace.id) {
+          AsanaWorkspaces.upsert({
+            id: workspace.id
+          }, {
+            $set: {
+              id: workspace.id,
+              name: workspace.name
+            }
+          });
+        }
+      });
+    },
+    asanaTasks: function asanaTasks(asana_user, modified_since) {
+      var asanaClient = Meteor.asanaClient();
+
+      if (!asana_user) {
+        asana_user = 'me';
+      }
+
+      if (!modified_since) {
+        modified_since = new Date('2015-09-05').toISOString();
+      }
+
+      var workspaces = AsanaWorkspaces.find({});
+      _.each(workspaces.fetch(), function(workspace) {
+        var tasks = Async.runSync(function(done) {
+          asanaClient.workspaces.tasks({
+            workspace: workspace.id,
+            assignee: asana_user,
+            modified_since: modified_since
+          }, function(err, tasks) {
+            done(null, tasks);
+          });
+        });
+
+        _.each(tasks.result, function(task) {
+          if (task.id && task.name) {
+            AsanaTasks.upsert({
+              id: task.id,
+            },{
+              id: task.id,
+              workspace: workspace.id,
+              workspace_id: workspace._id,
+              name: task.name,
+              title: task.name
+            });
+
+            Meteor.call('asanaTaskDetail', task.id);
+          }
+        });
+      })
+
+      console.log('Done importing');
+      Meteor.users.update({
+        _id: Meteor.userId()
+      },{
+        $set: {
+          asana_sync: new Date()
+        }
+      });
+    },
+    asanaTaskDetail: function asanaTaskDetail(task_id) {
+      var asanaClient = Meteor.asanaClient();
+      var taskDetail = Async.runSync(function(done) {
+        asanaClient.tasks.get(task_id, function(err, taskDetail) {
+          done(null, taskDetail);
+        });
+      });
+
+      if (taskDetail.result.id) {
+        console.log("inporting " + taskDetail.result.id  + " for workspace " + taskDetail.result.workspace.name);
+        AsanaTasks.upsert({
+          id: taskDetail.result.id,
+        },
+          taskDetail.result
+        );
+      }
+    },
+    asanaUpdate: function() {
+      var user = Meteor.user();
+      if (user.asana_sync) {
+        var date = user.asana_sync.toISOString();
+      }
+      else {
+        date = null;
+      }
+      Meteor.call('asanaTasks', 'me', date);
+    },
+    userLogin: function() {
+      var user = Meteor.user();
+      Meteor.users.update({
+        _id: Meteor.userId()
+      },{
+        $set: {
+          login_time: new Date(),
+          last_login: user.login_time,
+        }
+      });
+      Meteor.call('asanaUpdate');
+    }
+  });
+}
+
+// Public methods.
+Meteor.methods({
+
+});
+
+Router.route('/', function() {
+  // var auth = Meteor.call('asanaOauth');
+  // console.log(auth);
+  // if (auth && auth.redirect) {
+  //   Route.go(auth.redirect);
+  // }
+  this.render('Dashboard', {});
+});
+
+Router.route('/oauth', function () {
+  // var code = this.params.query.code;
+  // if (code) {
+  //   var asanaClient = Meteor.call('asanaClient');
+  //   asanaClient.app.accessTokenFromCode(code).then(function(credentials){
+  //     Session.set('AsanaToken', credentials.access_token);
+  //   });
+  // }
+  this.render('Oauth', {});
+});
